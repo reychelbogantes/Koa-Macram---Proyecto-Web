e952f67958f8993bff428b4bdd52c1aaeec1ace8
reychelbogantes <reychelfallasbogantes@gmail.com>
2026-05-27 17:06:06 -0600

    incorporacion de Render8

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(join(__dirname, 'koa macrame/db.json'));
+const router = jsonServer.router(join(__dirname, 'koa macrame', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
