49b33f3e88628cc753060d211430df8d51fb79a8
Rey <reychelfallasbogantes@gmail.com>
2026-05-27 13:00:04 -0600

    Add base for GitHub Pages
    Estos cambios son para crear GitHub Pages

--- koa macrame/vite.config.js
+++ koa macrame/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Koa-Macram---Proyecto-Web/',
   plugins: [react()],
-   server: {
+  server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
     },
