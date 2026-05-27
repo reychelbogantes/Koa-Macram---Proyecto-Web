4a9e8bd8c952db532060cb32ef3a77790d634314
reychelbogantes <reychelfallasbogantes@gmail.com>
2026-05-27 17:00:37 -0600

    incorporacion de Render7

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ const router = jsonServer.router(join(__dirname, 'koa macrame/db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
